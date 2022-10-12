0eaac2d0d453aa534eece3fbc81fd21f96659768
ebootdpr <36868128+ebootdpr@users.noreply.github.com>
2022-10-12 18:16:12 -0300

    Update gameloop.js

--- gameloop.js
+++ gameloop.js
@@ -114,7 +114,7 @@ listaParticulas[4]=anchuraGrafico/2
 listaParticulas[5]=alturaGrafico/2
 function update() {
     borrarPantalla()
-    rule(listaParticulas, listaParticulasVelocidades, 11, 1.2,1,300)
+    rule(listaParticulas, listaParticulasVelocidades, 1, 1.2,1,500)
     leyDeInercia(listaParticulas)
     dibujarParticulas(listaParticulas, 'red')
 
